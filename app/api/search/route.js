680118143bec8b94c22e41a787a821cc81b26a32
Curiosit <mailing.bot@gmail.com>
2023-11-05 18:16:22 +0100

    Update route.js

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,7 +1,7 @@
 export const GET = async (req) => {
     try {
         console.log("try fetching rewars query");
-        const query = "https://api.um.warszawa.pl/api/action/datastore_search/?resource_id=64b9d66c-d134-4a87-9f24-258676e9e498&limit=5&q"
+        const query = "https://api.um.warszawa.pl/api/action/datastore_search/?resource_id=64b9d66c-d134-4a87-9f24-258676e9e498&limit=5"
         console.log(query);
        const answers = await fetch(
         query
