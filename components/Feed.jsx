e0f6810ce5713676ad4a32d396f94220dc5248bc
Curiosit <mailing.bot@gmail.com>
2023-11-06 10:49:13 +0100

    removed API route

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -31,6 +31,7 @@ const ProductCardList = ({ data, handleTagClick}) => {
   
 }
 
+
 const Feed = () => {
   
   const [allPosts, setAllPosts] = useState([]);
@@ -57,7 +58,34 @@ const Feed = () => {
     
   }, [searchedResults]);
 
+  const fetchData = async (q) => {
+    try {
+      console.log("try fetching rewars query");
+      const query = "https://api.um.warszawa.pl/api/action/datastore_search/?resource_id=64b9d66c-d134-4a87-9f24-258676e9e498&limit=5&q=" + q
+      console.log(query);
+      const promise = (await fetch(query));
+      const answers = await promise.json();
+      //const data = JSON.parse(answers);
+      const data = JSON.parse(JSON.stringify(answers));
+      console.log(data.result.records);
+      return new Response(JSON.stringify(data.result.records),{ status: 200 } )
+
+    } catch (error) {
+      return new Response ("Nie udało się zadać pytania", {status: 500})
+    }
+  }
   const filterProducts = async (searchtext) => {
+    console.log("filter");
+    const response = await fetchData(searchtext);
+    
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    const result = JSON.parse(JSON.stringify(data));
+    console.log(result);
+    return result
+  };
+  const filterProducts2 = async (searchtext) => {
     console.log("filter");
     const response = await fetch('/api/search/'+searchtext);
     
